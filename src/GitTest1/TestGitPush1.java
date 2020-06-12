e7ab5209834d6daeb1c1ba41d5504bbf4483b8e5
murodzhon1 <murodkhakimov97@gmail.com>
2020-06-11 18:41:20 -0700

    first code push

--- src/GitTest1/TestGitPush1.java
+++ src/GitTest1/TestGitPush1.java
@@ -3,7 +3,8 @@ package GitTest1;
 public class TestGitPush1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("Syntax Technologies");
+		
 
 	}
 
